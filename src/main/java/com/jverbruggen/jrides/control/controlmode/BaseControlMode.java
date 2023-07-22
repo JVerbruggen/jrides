2c8bffbebf5ae14f3847b56491a719555d4accef
Jurjen Verbruggen <account@jurjenverbruggen.nl>
2023-07-22 11:21:28 +0200

    Cleanup BaseControlMode

--- src/main/java/com/jverbruggen/jrides/control/controlmode/BaseControlMode.java
+++ src/main/java/com/jverbruggen/jrides/control/controlmode/BaseControlMode.java
@@ -11,9 +11,7 @@ import com.jverbruggen.jrides.language.LanguageFileTag;
 import com.jverbruggen.jrides.models.entity.Player;
 import com.jverbruggen.jrides.models.properties.MinMaxWaitingTimer;
 import com.jverbruggen.jrides.models.ride.StationHandle;
-import com.jverbruggen.jrides.models.ride.coaster.train.Train;
 import com.jverbruggen.jrides.models.ride.coaster.train.Vehicle;
-import org.bukkit.Bukkit;
 
 public abstract class BaseControlMode implements ControlMode {
     protected final RideHandle rideHandle;
@@ -23,7 +21,6 @@ public abstract class BaseControlMode implements ControlMode {
     protected Player operator;
 
     protected final long tickInterval;
-    private boolean started;
 
     private int tickIntervalState;
 
