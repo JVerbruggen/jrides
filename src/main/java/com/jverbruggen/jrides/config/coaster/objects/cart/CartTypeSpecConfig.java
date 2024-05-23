04f748a296e83f25fa9eb3ca505ea3c354b6bd70
Sibren van Setten <sibrenvansetten@hotmail.nl>
2024-05-23 23:42:38 +0200

    WheelDistance config refactor

--- src/main/java/com/jverbruggen/jrides/config/coaster/objects/cart/CartTypeSpecConfig.java
+++ src/main/java/com/jverbruggen/jrides/config/coaster/objects/cart/CartTypeSpecConfig.java
@@ -31,10 +31,7 @@ public class CartTypeSpecConfig extends BaseConfig {
     public static CartTypeSpecConfig fromConfigurationSection(@Nullable ConfigurationSection configurationSection) {
         ModelConfig model = ModelConfig.fromConfigurationSection(getConfigurationSection(configurationSection, "model"));
         CartSeatsConfig seats = CartSeatsConfig.fromConfigurationSection(getConfigurationSection(configurationSection, "seats"));
-        if(configurationSection != null) {
-            int wheelDistance = configurationSection.getInt("wheelDistance", 0);
-            return new CartTypeSpecConfig(model, seats, wheelDistance);
-        }
-        return new CartTypeSpecConfig(model, seats, 0);
+        int wheelDistance = getInt(configurationSection, "wheelDistance", 0);
+        return new CartTypeSpecConfig(model, seats, wheelDistance);
     }
 }
