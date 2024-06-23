2339b28bdfa61e5bb52c4b7df17ac0ab4e4fedde
Jurjen Verbruggen <account@jurjenverbruggen.nl>
2024-06-23 11:07:28 +0200

    Added copyright notices

--- src/main/java/com/jverbruggen/jrides/models/menu/Menu.java
+++ src/main/java/com/jverbruggen/jrides/models/menu/Menu.java
@@ -1,3 +1,20 @@
+/************************************************************************************************************
+ * GPLv3 License                                                                                            *
+ *                                                                                                          *
+ * Copyright (c) 2024-2024 JVerbruggen                                                                      *
+ * https://github.com/JVerbruggen/jrides                                                                    *
+ *                                                                                                          *
+ * This software is protected under the GPLv3 license,                                                      *
+ * that can be found in the project's LICENSE file.                                                         *
+ *                                                                                                          *
+ * In short, permission is hereby granted that anyone can copy, modify and distribute this software.        *
+ * You have to include the license and copyright notice with each and every distribution. You can use       *
+ * this software privately or commercially. Modifications to the code have to be indicated, and             *
+ * distributions of this code must be distributed with the same license, GPLv3. The software is provided    *
+ * without warranty. The software author or license can not be held liable for any damages                  *
+ * inflicted by the software.                                                                               *
+ ************************************************************************************************************/
+
 package com.jverbruggen.jrides.models.menu;
 
 import com.jverbruggen.jrides.api.JRidesPlayer;
