ff38424d49b003694961f4cc571ce446d62f1249
Jurjen Verbruggen <account@jurjenverbruggen.nl>
2024-06-23 11:09:17 +0200

    Added copyright notices to tools

--- tools/jrides_blender_export_anim_plugin.py
+++ tools/jrides_blender_export_anim_plugin.py
@@ -1,5 +1,3 @@
-# Script from ottle, with some changes specific to jrides.
-
 bl_info = {
     "name": "JRides Export Animation",
     "author": "ottle,jverbruggen",
@@ -12,6 +10,21 @@ bl_info = {
     "support": 'COMMUNITY',
     "category": "Import-Export",
 }
+
+#  GPLv3 License
+#
+#  Copyright (c) 2024-2024 JVerbruggen
+#  https://github.com/JVerbruggen/jrides
+#
+#  This software is protected under the GPLv3 license,
+#  that can be found in the project's LICENSE file.
+#
+#  In short, permission is hereby granted that anyone can copy, modify and distribute this software.
+#  You have to include the license and copyright notice with each and every distribution. You can use
+#  this software privately or commercially. Modifications to the code have to be indicated, and
+#  distributions of this code must be distributed with the same license, GPLv3. The software is provided
+#  without warranty. The software author or license can not be held liable for any damages
+#  inflicted by the software.
 
 import os
 import bpy
